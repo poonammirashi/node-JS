5f13c439731d3a0b432484b82f06a23c2008eb8d
poonammirashi <poonam200023@gmail.com>
2023-04-29 00:02:38 -0700

    message on the top

--- app.js
+++ app.js
@@ -8,20 +8,31 @@ const server = http.createServer((req, res) => {
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
         res.write('<body><form action="/home" method="POST"><input type="text" name="home"><button type="submit">home</button></form></body>');
-        res.write('<body><form action="/about" method="POST"><input type="text" name="aboutus"><button type="submit">about us</button></form></body>');
-        res.write('<body><form action="/node" method="POST"><input type="text" name="node"><button type="submit">node</button></form></body>');
+        // res.write('<body><form action="/about" method="POST"><input type="text" name="aboutus"><button type="submit">about us</button></form></body>');
+        // res.write('<body><form action="/node" method="POST"><input type="text" name="node"><button type="submit">node</button></form></body>');
         res.write('</html>');
         return res.end();
 
     }
     if (url === '/home' && method === 'POST') {
-        console.log('poonam gautam')
-        res.setHeader('location', '/');
-        res.write('<html>');
-        res.write('<head><title>my first page</title></head>');
-        res.write('<body><h1>wellcome to  home</h1></body>')
-        res.write('</html>');
-        return res.end();
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
+        req.on("end", () => {
+            const parsedbody = Buffer.concat(body).toString();
+            // console.log(parsedbody);
+            const message = parsedbody.split("=")[1];
+            fs.writeFile("home.txt", message, err => {
+                res.setHeader("locatoin", "/");
+                res.write(`<body><h1>${message}</h1><form action="/home" method="POST"><input type="text" name="home"><button type="submit">home</button></form></body>`);
+                res.statusCode = 302;
+                return res.end();
+            });
+        })
+
+
     }
     if (url === '/about' && method === 'POST') {
         console.log("poonam")
@@ -38,7 +49,7 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>my second page</title></head>');
         res.write('<body><h1>wellcome to  my node js server</h1></body>')
         res.write('</html>');
-        res.end();
+        return res.end();
         // process.exit();
     }
 })
