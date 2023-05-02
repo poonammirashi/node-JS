53e2bbd568c12c4757594830d165415e94f44181
poonammirashi <poonam200023@gmail.com>
2023-05-02 05:14:49 -0700

    express-middleware

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http');
 
-const routes = require("./routes");
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
-server.listen(4000)
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=> {
+    console.log("in the middleware!");
+    next(); // allows the request continue to the next middleware in the line
+})
+
+app.use((req, res, next)=> {
+    console.log("in the middleware!");
+    // res.send({ key1: value });
+    res.send("<h1>hii</h1>");
+    
+});
+
+app.listen(4000);
