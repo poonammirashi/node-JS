0206d2069c647eeaa50d6ad5d09fa90cfa9e67ef
poonammirashi <poonam200023@gmail.com>
2023-05-02 22:53:01 -0700

    parsing body

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-
 const express = require('express');
+const bodyparser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log("in the middleware!");
-    next(); // allows the request continue to the next middleware in the line
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use("/add-product", (req, res, next)=> {
+    // console.log("in the middleware!");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">add product</button></form>');
+});
+
+app.post("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 })
 
-app.use((req, res, next)=> {
-    console.log("in the middleware!");
-    // res.send({ key1: value });
-    res.send("<h1>hii</h1>");
-    
+app.use("/", (req, res, next)=> {
+    // console.log("in the another  middleware!");
+    res.send("<h1> HEllo from express</h1>");
 });
+
 
 app.listen(4000);
