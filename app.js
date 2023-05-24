d6ed055421d16572a740e67cc494e83cd1649210
poonammirashi <poonam200023@gmail.com>
2023-05-24 04:29:09 -0700

    using Sequelize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -13,6 +13,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+// const sequelize = require('./util/database');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,4 +23,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+.sync()
+.then(result => {
+    // console.log(result);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+});
